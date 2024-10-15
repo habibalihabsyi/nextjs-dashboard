9f8e3aad353e44d374522d1b02811eefdaa3fed5
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2024-10-15 22:08:59 +0700

    fix: lint

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -53,6 +53,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.error(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -85,6 +86,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error(error);
     return {
       message: 'Database Error: Failed to Update Invoice.',
     };
@@ -102,6 +104,7 @@ export async function deleteInvoice(id: string) {
       message: 'Deleted Invoice.'
     };
   } catch (error) {
+    console.error(error);
     return {
       message: 'Database Error: Failed to Delete Invoice.',
     };
