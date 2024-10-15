6077d45f8a6f11049ffba1f14029fc8bc2042dd7
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2024-10-15 22:17:52 +0700

    fix

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,7 +8,7 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
-import { createInvoice } from "@/app/lib/action";
+import { createInvoice, State } from "@/app/lib/action";
 import { useActionState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
